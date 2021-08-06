7d5dab0093afef8145951b7e1dc634dbd547d4d1
Groofi <varchikcreed@mail.ru>
2021-08-06 21:45:39 +0300

    Home_8

---
+++ package_messanger_server/whl/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="Server",
+      py_modules=["formserver"],
+      install_requires=["greenlet==1.1.0",
+                        "PyQt5==5.15.4",
+                        "PyQt5-Qt5==5.15.2",
+                        "PyQt5-sip==12.9.0",
+                        "SQLAlchemy==1.4.22"],
+      packages=["src","src/log"],
+      )
